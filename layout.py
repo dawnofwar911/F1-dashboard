b46690bc9875a203250e334f0259da2199ac3bb8
dawnofwar911 <alexander-daniels@hotmail.co.uk>
2025-05-25 19:57:44 +0100

    added store

--- layout.py
+++ layout.py
@@ -46,6 +46,7 @@ def create_layout():
         dcc.Store(id='current-track-layout-cache-key-store'),
         dcc.Store(id='track-map-figure-version-store'),
         dcc.Store(id='track-map-yellow-key-store', storage_type='memory', data=""),
+        dcc.Store(id='clicked-car-driver-number-store', storage_type='memory'),
         dcc.Interval(id='clientside-update-interval', interval=1250, n_intervals=0, disabled=True)
     ])
 
